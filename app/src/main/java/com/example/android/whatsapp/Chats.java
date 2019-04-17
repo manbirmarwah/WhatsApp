e83dede7883be994f171eae763efef0b41214a39
Manbir <marwahmanbirsingh@gmail.com>
2019-04-17 17:53:42 +0530

    Updated app

--- app/src/main/java/com/example/android/whatsapp/Chats.java
+++ app/src/main/java/com/example/android/whatsapp/Chats.java
@@ -1,11 +1,12 @@
 package com.example.android.whatsapp;
-
 import android.os.Bundle;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
